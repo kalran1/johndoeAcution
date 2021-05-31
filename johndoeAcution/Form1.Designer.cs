e6261c2401bfa1e7dea3ac56d9a6be0a998b00a6
조민혁 <kaev.owen@gmail.com>
2021-05-31 10:36:36 +0900

    tt

--- johndoeAcution/Form1.Designer.cs
+++ johndoeAcution/Form1.Designer.cs
@@ -40,6 +40,9 @@ namespace johndoeAcution
             this.txtItem = new System.Windows.Forms.TextBox();
             this.btnBuy = new System.Windows.Forms.Button();
             this.gruopBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -48,6 +51,7 @@ namespace johndoeAcution
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.gruopBox1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -64,7 +68,7 @@ namespace johndoeAcution
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(712, 383);
+            this.dataGridView1.Size = new System.Drawing.Size(815, 275);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -118,9 +122,9 @@ namespace johndoeAcution
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(227, 29);
+            this.btnSearch.Location = new System.Drawing.Point(227, 23);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(127, 32);
+            this.btnSearch.Size = new System.Drawing.Size(118, 23);
             this.btnSearch.TabIndex = 1;
             this.btnSearch.Text = "검색";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -128,17 +132,17 @@ namespace johndoeAcution
             // 
             // txtItem
             // 
-            this.txtItem.Location = new System.Drawing.Point(6, 29);
+            this.txtItem.Location = new System.Drawing.Point(6, 24);
             this.txtItem.Name = "txtItem";
-            this.txtItem.Size = new System.Drawing.Size(215, 30);
+            this.txtItem.Size = new System.Drawing.Size(215, 25);
             this.txtItem.TabIndex = 2;
             this.txtItem.TextChanged += new System.EventHandler(this.txtItem_TextChanged);
             // 
             // btnBuy
             // 
-            this.btnBuy.Location = new System.Drawing.Point(227, 71);
+            this.btnBuy.Location = new System.Drawing.Point(227, 55);
             this.btnBuy.Name = "btnBuy";
-            this.btnBuy.Size = new System.Drawing.Size(127, 35);
+            this.btnBuy.Size = new System.Drawing.Size(118, 23);
             this.btnBuy.TabIndex = 3;
             this.btnBuy.Text = "구매";
             this.btnBuy.UseVisualStyleBackColor = true;
@@ -148,15 +152,47 @@ namespace johndoeAcution
             this.gruopBox1.Controls.Add(this.txtItem);
             this.gruopBox1.Controls.Add(this.btnBuy);
             this.gruopBox1.Controls.Add(this.btnSearch);
-            this.gruopBox1.Font = new System.Drawing.Font("굴림", 12F);
-            this.gruopBox1.Location = new System.Drawing.Point(12, 12);
+            this.gruopBox1.Location = new System.Drawing.Point(321, 12);
             this.gruopBox1.Name = "gruopBox1";
-            this.gruopBox1.Size = new System.Drawing.Size(370, 123);
+            this.gruopBox1.Size = new System.Drawing.Size(370, 100);
             this.gruopBox1.TabIndex = 4;
             this.gruopBox1.TabStop = false;
             this.gruopBox1.Text = "물건 정보 검색 및 구매";
             // 
-            // comboBox1
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(178, 26);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(109, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "로그인";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLogin);
+            this.groupBox1.Controls.Add(this.btnLogin);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "회원 정보";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(6, 24);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(166, 25);
+            this.txtLogin.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "id";
+            this.Column1.HeaderText = "ID";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
             // 
             // Column2
             // 
@@ -164,6 +200,7 @@ namespace johndoeAcution
             this.Column2.HeaderText = "이름";
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
             // 
             // Column3
             // 
@@ -171,6 +208,7 @@ namespace johndoeAcution
             this.Column3.HeaderText = "제조사";
             this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
             // 
             // Column4
             // 
@@ -178,26 +216,30 @@ namespace johndoeAcution
             this.Column4.HeaderText = "가격";
             this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
             // 
             // Column5
             // 
             this.Column5.DataPropertyName = "smallId";
-            this.Column5.HeaderText = "소분류";
+            this.Column5.HeaderText = "소분류코드";
             this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
             // 
             // Column6
             // 
-            this.Column6.DataPropertyName = "buy";
+            this.Column6.DataPropertyName = "Buy";
             this.Column6.HeaderText = "구매 여부";
             this.Column6.MinimumWidth = 6;
             this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(736, 532);
+            this.ClientSize = new System.Drawing.Size(870, 480);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.gruopBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -206,6 +248,8 @@ namespace johndoeAcution
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.gruopBox1.ResumeLayout(false);
             this.gruopBox1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -223,6 +267,9 @@ namespace johndoeAcution
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLogin;
     }
 }
 
--- johndoeAcutionServer/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace johndoeAcutionServer.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
